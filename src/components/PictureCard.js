ae9d0c92dc036dc5902354a347f7c1d8f3311132
felixgren <33127919+felixgren@users.noreply.github.com>
2021-03-07 20:48:25 +0100

    Optimized Invite for desktop

--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import desktop from "../utils/breakpoints";
 
 const Wrapper = styled.div``;
 
@@ -11,11 +12,17 @@ const Paragraph = styled.p`
   font-size: ${props => props.size || '16px'};
   padding: 24px 60px;
   text-align: left;
+
+  ${desktop} {
+    font-size: 28px;
+    width: 500px;
+    margin: 20% 10%;
+  }
 `;
 
-const PictureCard = ({ imgSrc, imgAlt, text, fontSize }) => (
+const PictureCard = ({ imgSrc, imgAlt, text, fontSize, height }) => (
     <Wrapper>
-      <Image src={imgSrc} alt={imgAlt} />
+      <Image height={height} src={imgSrc} alt={imgAlt} />
       <Paragraph size={fontSize}>{text}</Paragraph>
     </Wrapper>
 );
