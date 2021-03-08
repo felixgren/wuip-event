7f533b3acf48ce003be04c24db6ddc46362b644e
felixgren <33127919+felixgren@users.noreply.github.com>
2021-03-08 22:16:55 +0100

    Added prettier

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,85 +1,85 @@
-import styled from "styled-components";
-import heroimgMob from "../images/iphone-hero.png";
-import heroimgDesk from "../images/desktop-hero.png";
-import ArrowIcon from "../icons/ArrowIcon";
-import desktop from "../utils/breakpoints";
+import styled from 'styled-components';
+import heroimgMob from '../images/iphone-hero.png';
+import heroimgDesk from '../images/desktop-hero.png';
+import ArrowIcon from '../icons/ArrowIcon';
+import desktop from '../utils/breakpoints';
 
 const HeroWrapper = styled.div`
-  position: relative;
+    position: relative;
 `;
 
 const Wrapper = styled.div`
-  position: absolute;
-  background-color: #ffffff5c;
-  top: 122px;
-  left: 19px;
-  width: 290px;
+    position: absolute;
+    background-color: #ffffff5c;
+    top: 122px;
+    left: 19px;
+    width: 290px;
 
-  ${desktop} {
-    display: flex;
-    flex-wrap: wrap;
-    padding: 20px;
-    width: 890px;
-    left: 20%;
-    top: 300px;
-  }
+    ${desktop} {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 20px;
+        width: 890px;
+        left: 20%;
+        top: 300px;
+    }
 `;
 
 const Paragraph = styled.p`
-  font-size: 20px;
-  text-align: center;
-  padding: 8px;
+    font-size: 20px;
+    text-align: center;
+    padding: 8px;
 
-  ${desktop} {
-    font-size: 40px;
-  }
+    ${desktop} {
+        font-size: 40px;
+    }
 `;
 
 const HeroImageMob = styled.img`
-  width: 100%;
+    width: 100%;
 
-  ${desktop} {
-    display: none;
-  }
+    ${desktop} {
+        display: none;
+    }
 `;
 
 const HeroImageDesk = styled.img`
-  width: 100%;
-  display: none;
-  
-  ${desktop} {
-    display: initial;
-  }
+    width: 100%;
+    display: none;
+
+    ${desktop} {
+        display: initial;
+    }
 `;
 
 const StyledArrowIcon = styled(ArrowIcon)`
-  margin-top: -54px;
-  display: block;
+    margin-top: -54px;
+    display: block;
 
-  ${desktop} {
-    transition: 0.2s;
+    ${desktop} {
+        transition: 0.2s;
 
-    & > path {
-      fill: #56DDA8;
-      stroke: transparent;
+        & > path {
+            fill: #56dda8;
+            stroke: transparent;
+        }
     }
-  }
 `;
 
 const Hero = () => {
-  return (
-    <HeroWrapper>
-      <Wrapper>
-        <Paragraph>SUPER MARIO 3D WORLD</Paragraph>
-        <Paragraph>+</Paragraph>
-        <Paragraph>BOWSERS FURY: A JOYFUL CELEBRATION</Paragraph>
-      </Wrapper>
-      <HeroImageMob src={heroimgMob} alt="amazing" />
-      <HeroImageDesk src={heroimgDesk} alt="amazing" />
-      <StyledArrowIcon stroke="black" color="none" w="100%" h="24px" />
-      <StyledArrowIcon stroke="black" color="none" w="100%" h="24px" />
-    </HeroWrapper>
-  );
+    return (
+        <HeroWrapper>
+            <Wrapper>
+                <Paragraph>SUPER MARIO 3D WORLD</Paragraph>
+                <Paragraph>+</Paragraph>
+                <Paragraph>BOWSERS FURY: A JOYFUL CELEBRATION</Paragraph>
+            </Wrapper>
+            <HeroImageMob src={heroimgMob} alt="amazing" />
+            <HeroImageDesk src={heroimgDesk} alt="amazing" />
+            <StyledArrowIcon stroke="black" color="none" w="100%" h="24px" />
+            <StyledArrowIcon stroke="black" color="none" w="100%" h="24px" />
+        </HeroWrapper>
+    );
 };
 
 export default Hero;
